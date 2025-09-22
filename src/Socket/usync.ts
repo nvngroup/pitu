d950ba2a6dccaef501cc8b3035d72b396ecca548
Bruno Cardoso <brunocgc@nvngroup.com.br>
2025-09-22 16:50:15 -0300

    refactor: remove device sync functionality and clean up executeUSyncQuery

--- src/Socket/usync.ts
+++ src/Socket/usync.ts
@@ -12,11 +12,13 @@ export const makeUSyncSocket = (config: SocketConfig) => {
 		query,
 	} = sock
 
-	const executeUSyncQuery = async(usyncQuery: USyncQuery) => {
-		if(usyncQuery.protocols.length === 0) {
+	const executeUSyncQuery = async (usyncQuery: USyncQuery) => {
+		if (usyncQuery.protocols.length === 0) {
 			throw new Boom('USyncQuery must have at least one protocol')
 		}
 
+		// TODO: validate users, throw WARNING on no valid users
+		// variable below has only validated users
 		const validUsers = usyncQuery.users
 
 		const userNodes = validUsers.map((user) => {
@@ -42,7 +44,6 @@ export const makeUSyncSocket = (config: SocketConfig) => {
 			attrs: {},
 			content: usyncQuery.protocols.map((a) => a.getQueryElement())
 		}
-
 		const iq = {
 			tag: 'iq',
 			attrs: {
@@ -59,8 +60,6 @@ export const makeUSyncSocket = (config: SocketConfig) => {
 						sid: generateMessageTag(),
 						last: 'true',
 						index: '0',
-						target: 'devices',
-						sync: 'full'
 					},
 					content: [
 						queryNode,
@@ -72,58 +71,11 @@ export const makeUSyncSocket = (config: SocketConfig) => {
 
 		const result = await query(iq)
 
-		// Force device sync after query
-		await ensureDeviceSync()
-
 		return usyncQuery.parseUSyncQueryResult(result)
-	}
-
-	const ensureDeviceSync = async() => {
-		try {
-			const deviceSyncIq = {
-				tag: 'iq',
-				attrs: {
-					to: S_WHATSAPP_NET,
-					type: 'get',
-					xmlns: 'usync',
-				},
-				content: [
-					{
-						tag: 'usync',
-						attrs: {
-							context: 'interactive',
-							mode: 'query',
-							sid: generateMessageTag(),
-							last: 'true',
-							index: '0',
-						},
-						content: [
-							{
-								tag: 'query',
-								attrs: {},
-								content: [
-									{
-										tag: 'devices',
-										attrs: {
-											version: '2',
-										},
-									}
-								]
-							}
-						]
-					}
-				],
-			}
-
-			await query(deviceSyncIq)
-		} catch(error) {
-			console.warn('Device sync failed:', error)
-		}
 	}
 
 	return {
 		...sock,
 		executeUSyncQuery,
-		ensureDeviceSync,
 	}
 }
