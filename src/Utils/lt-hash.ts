02af88cdd4bcd6e15c31e0950d555880c538b74c
Bruno Cardoso <brunocgc@nvngroup.com.br>
2025-10-30 14:31:53 -0300

    refactor: improve LTHash code readability

--- src/Utils/lt-hash.ts
+++ src/Utils/lt-hash.ts
@@ -6,56 +6,70 @@ import { hkdf } from './crypto'
  * if the same series of mutations was made sequentially.
  */
 
-const o = 128
+const HASH_SIZE = 128
 
-class d {
+class LTHash {
 
 	salt: string
 
-	constructor(e: string) {
-		this.salt = e
+	constructor(salt: string) {
+		this.salt = salt
 	}
-	add(e, t) {
-		var r = this
-		for(const item of t) {
-			e = r._addSingle(e, item)
+
+	add(hash: ArrayBuffer | Promise<ArrayBuffer>, items: string[] | ArrayBuffer[]) {
+		let result = hash
+		for(const item of items) {
+			result = this._addSingle(result, item)
 		}
 
-		return e
+		return result
 	}
-	subtract(e, t) {
-		var r = this
-		for(const item of t) {
-			e = r._subtractSingle(e, item)
+
+	subtract(hash: ArrayBuffer | Promise<ArrayBuffer>, items: string[] | ArrayBuffer[]) {
+		let result = hash
+		for(const item of items) {
+			result = this._subtractSingle(result, item)
 		}
 
-		return e
+		return result
 	}
-	subtractThenAdd(e, t, r) {
-		var n = this
-		return n.add(n.subtract(e, r), t)
-	}
-	async _addSingle(e, t) {
-		var r = this
-		const n: ArrayBuffer = new Uint8Array(await hkdf(Buffer.from(t), o, { info: r.salt })).buffer
-		return r.performPointwiseWithOverflow(await e, n, ((e, t) => e + t))
-	}
-	async _subtractSingle(e, t) {
-		var r = this
 
-		const n: ArrayBuffer = new Uint8Array(await hkdf(Buffer.from(t), o, { info: r.salt })).buffer
-		return r.performPointwiseWithOverflow(await e, n, ((e, t) => e - t))
+	subtractThenAdd(hash: ArrayBuffer | Promise<ArrayBuffer>, addItems: string[] | ArrayBuffer[], subtractItems: string[] | ArrayBuffer[]) {
+		return this.add(this.subtract(hash, subtractItems), addItems)
 	}
-	performPointwiseWithOverflow(e, t, r) {
-		const n = new DataView(e)
-		  , i = new DataView(t)
-		  , a = new ArrayBuffer(n.byteLength)
-		  , s = new DataView(a)
-		for(let e = 0; e < n.byteLength; e += 2) {
-			s.setUint16(e, r(n.getUint16(e, !0), i.getUint16(e, !0)), !0)
+
+	async _addSingle(hash: ArrayBuffer | Promise<ArrayBuffer>, item: string | ArrayBuffer) {
+		const itemBuffer = typeof item === 'string' ? Buffer.from(item) : Buffer.from(item)
+		const itemHash: ArrayBuffer = new Uint8Array(await hkdf(itemBuffer, HASH_SIZE, { info: this.salt })).buffer
+		return this.performPointwiseWithOverflow(await hash, itemHash, ((a, b) => a + b))
+	}
+
+	async _subtractSingle(hash: ArrayBuffer | Promise<ArrayBuffer>, item: string | ArrayBuffer) {
+		const itemBuffer = typeof item === 'string' ? Buffer.from(item) : Buffer.from(item)
+		const itemHash: ArrayBuffer = new Uint8Array(await hkdf(itemBuffer, HASH_SIZE, { info: this.salt })).buffer
+		return this.performPointwiseWithOverflow(await hash, itemHash, ((a, b) => a - b))
+	}
+
+	performPointwiseWithOverflow(
+		buffer1: ArrayBuffer,
+		buffer2: ArrayBuffer,
+		operation: (a: number, b: number) => number
+	) {
+		const view1 = new DataView(buffer1)
+		const view2 = new DataView(buffer2)
+		const resultBuffer = new ArrayBuffer(view1.byteLength)
+		const resultView = new DataView(resultBuffer)
+
+		for(let index = 0; index < view1.byteLength; index += 2) {
+			resultView.setUint16(
+				index,
+				operation(view1.getUint16(index, true), view2.getUint16(index, true)),
+				true
+			)
 		}
 
-		return a
+		return resultBuffer
 	}
 }
-export const LT_HASH_ANTI_TAMPERING = new d('WhatsApp Patch Integrity')
+
+export const LT_HASH_ANTI_TAMPERING = new LTHash('WhatsApp Patch Integrity')
