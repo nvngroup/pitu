ad41746591df921c347db75d1f718d9aa204dc51
Bruno Cardoso <brunocgc@nvngroup.com.br>
2025-11-07 10:31:02 -0300

    refactor: adicionar configuração de histórico de sincronização e prefixos de assinatura de conta e dispositivo

--- src/Utils/validate-connection.ts
+++ src/Utils/validate-connection.ts
@@ -1,9 +1,14 @@
 import { Boom } from '@hapi/boom'
 import { createHash } from 'crypto'
 import { waproto } from '../../WAProto'
-import { KEY_BUNDLE_TYPE } from '../Defaults'
+import {
+	KEY_BUNDLE_TYPE,
+	WA_ADV_ACCOUNT_SIG_PREFIX,
+	WA_ADV_DEVICE_SIG_PREFIX,
+	WA_ADV_HOSTED_ACCOUNT_SIG_PREFIX
+} from '../Defaults'
 import type { AuthenticationCreds, SignalCreds, SignalIdentity, SocketConfig } from '../Types'
-import { BinaryNode, getBinaryNodeChild, jidDecode, S_WHATSAPP_NET } from '../WABinary'
+import { type BinaryNode, getBinaryNodeChild, jidDecode, S_WHATSAPP_NET } from '../WABinary'
 import { Curve, hmacSign } from './crypto'
 import { encodeBigEndian } from './generics'
 import { createSignalIdentity } from './signal'
@@ -84,6 +89,23 @@ export const generateRegistrationNode = (
 		os: config.browser[0],
 		platformType: getPlatformType(config.browser[1]),
 		requireFullSync: config.syncFullHistory,
+		historySyncConfig: {
+			storageQuotaMb: 569150,
+			inlineInitialPayloadInE2EeMsg: false,
+			supportCallLogHistory: false,
+			supportBotUserAgentChatHistory: true,
+			supportCagReactionsAndPolls: true,
+			supportBizHostedMsg: true,
+			supportRecentSyncChunkMessageCountTuning: true,
+			supportHostedGroupMsg: true,
+			supportFbidBotChatHistory: true,
+			supportMessageAssociation: true
+		},
+		version: {
+			primary: 10,
+			secondary: 15,
+			tertiary: 7
+		}
 	}
 
 	const companionProto: Uint8Array = waproto.DeviceProps.encode(companion).finish()
@@ -125,11 +147,15 @@ export const configureSuccessfulPairing = (
 
 	const bizName: string | undefined = businessNode?.attrs.name
 	const jid: string = deviceNode.attrs.jid
+	const lid: string = deviceNode.attrs.lid
 
 	const { details, hmac, accountType } = waproto.ADVSignedDeviceIdentityHMAC.decode(deviceIdentityNode.content as Buffer)
-	const isHostedAccount: boolean = accountType !== undefined && accountType === waproto.ADVEncryptionType.HOSTED
 
-	const hmacPrefix: Buffer = isHostedAccount ? Buffer.from([6, 5]) : Buffer.alloc(0)
+	let hmacPrefix: Buffer = Buffer.from([])
+	if(accountType !== undefined && accountType === waproto.ADVEncryptionType.HOSTED) {
+		hmacPrefix = WA_ADV_HOSTED_ACCOUNT_SIG_PREFIX
+	}
+
 	const advSign: Buffer = hmacSign(Buffer.concat([hmacPrefix, details!]), Buffer.from(advSecretKey, 'base64'))
 	if(Buffer.compare(hmac!, advSign) !== 0) {
 		throw new Boom('Invalid account signature')
@@ -137,19 +163,28 @@ export const configureSuccessfulPairing = (
 
 	const account: waproto.ADVSignedDeviceIdentity = waproto.ADVSignedDeviceIdentity.decode(details!)
 	const { accountSignatureKey, accountSignature, details: deviceDetails } = account
-	const accountMsg: Buffer = Buffer.concat([ Buffer.from([6, 0]), deviceDetails!, signedIdentityKey.public ])
+
+	const deviceIdentity: waproto.ADVDeviceIdentity = waproto.ADVDeviceIdentity.decode(deviceDetails!)
+
+	const accountSignaturePrefix =
+		deviceIdentity.deviceType === waproto.ADVEncryptionType.HOSTED
+			? WA_ADV_HOSTED_ACCOUNT_SIG_PREFIX
+			: WA_ADV_ACCOUNT_SIG_PREFIX
+	const accountMsg: Buffer = Buffer.concat([accountSignaturePrefix, deviceDetails!, signedIdentityKey.public])
 	if(!Curve.verify(accountSignatureKey!, accountMsg, accountSignature!)) {
 		throw new Boom('Failed to verify account signature')
 	}
 
-	const devicePrefix: Buffer = isHostedAccount ? Buffer.from([6, 6]) : Buffer.from([6, 1])
-	const deviceMsg: Buffer = Buffer.concat([devicePrefix, deviceDetails!, signedIdentityKey.public, accountSignatureKey!])
+	const deviceMsg: Buffer = Buffer.concat([
+		WA_ADV_DEVICE_SIG_PREFIX,
+		deviceDetails!,
+		signedIdentityKey.public,
+		accountSignatureKey!
+	])
 	account.deviceSignature = Curve.sign(signedIdentityKey.private, deviceMsg)
 
-	const identity: SignalIdentity = createSignalIdentity(jid, accountSignatureKey!)
+	const identity: SignalIdentity = createSignalIdentity(lid, accountSignatureKey!)
 	const accountEnc: Uint8Array = encodeSignedDeviceIdentity(account, false)
-
-	const deviceIdentity: waproto.ADVDeviceIdentity = waproto.ADVDeviceIdentity.decode(account.details!)
 
 	const reply: BinaryNode = {
 		tag: 'iq',
@@ -175,7 +210,7 @@ export const configureSuccessfulPairing = (
 
 	const authUpdate: Partial<AuthenticationCreds> = {
 		account,
-		me: { id: jid, name: bizName },
+		me: { id: jid, name: bizName, lid: lid },
 		signalIdentities: [
 			...(signalIdentities || []),
 			identity
